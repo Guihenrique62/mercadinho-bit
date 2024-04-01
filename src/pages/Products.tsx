46e3aad8c8d6e48988551893b733e74b543c9f9c
Guilherme Henrique <guilhermeportosantos1@gmail.com>
2024-03-31 23:21:12 -0300

    fix importacao componente header

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "../components/header";
+import Header from "../components/Header";
 import { Container } from "reactstrap";
 import ProductsList from "../components/ProductsList";
 import { GetStaticProps, NextPage } from "next";
